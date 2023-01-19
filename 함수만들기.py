aa83c8b068118ba9e39a6693848f943edc7e7f00
hyunin3 <stock4share2@gmail.com>
2023-01-19 14:17:45 +0900

    yeah

---
+++ 함수만들기.py
@@ -0,0 +1,48 @@
+# numbers=[1,2,3,4]      #sum()없이 주어진 리스트의 합을 구하기
+# sum = 0 
+# for i in numbers:
+#     sum = sum+i
+# print(sum)    
+
+# num = int(input())      #입력받은 수까지의 합을 구하는 함수
+# def sum_numbers(num):
+#     sum = 0
+#     for i in range(1, num+1):
+#         sum = sum+i
+#     return sum    
+
+# print(sum_numbers(num))
+
+
+''''''
+
+#  numbers=[1,2,3,4,0,3,10,7]  #유사 max만들기
+# i=0
+# for number in numbers:
+#     if i<=number:
+#         i=number 
+# print(i)        
+
+
+''''''
+
+# def my_func(n):              #map연습
+#     return n*10
+
+# my_lst=[1,2,3,4,5]
+
+# # for i in my_lst:
+# #     lst=[my_func(i)]
+# #     print(lst)    
+
+# rlt=map(my_func,my_lst)
+# print(list(rlt))
+
+
+''''''
+
+# numbers = [1,2,3,4]         #len만들기
+# cnt=  0
+# for i in numbers:
+#     cnt=cnt+1
+# print(cnt) 
