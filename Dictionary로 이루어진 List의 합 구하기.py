9d6f14f466d9d8ff0929b9cd235ba20306e73b4c
hyunin3 <stock4share2@gmail.com>
2023-01-22 22:10:44 +0900

    practice for exam

--- Dictionary로 이루어진 List의 합 구하기.py
+++ Dictionary로 이루어진 List의 합 구하기.py
@@ -4,6 +4,8 @@
 #dict_list_sum[0]['age']+dict_list_sum[1]['age']
 
 #dic의 데이터를 리스트 안에서 for 라는 반복으로 가져와야
+list_a = [{'name': 'kim', 'age': 12},
+               {'name': 'lee', 'age': 4}]
 def dict_list_sum(lst):
     sum = 0
     for dic in lst:
@@ -13,8 +15,5 @@ def dict_list_sum(lst):
     return sum
 #print(dic['age'])
 
-print(dict_list_sum([{'name': 'kim', 'age': 12},
-                    {'name': 'lee', 'age': 4}]))        
+print(dict_list_sum(list_a))        
 
-#dic = [{'name': 'kim', 'age': 12},
-               {'name': 'lee', 'age': 4}]
---
+++ a.py
@@ -0,0 +1,7 @@
+def get_secret_number(words):
+    result = 0
+    for word in words:
+        result = result + ord(word)
+    return result    
+
+print(get_secret_number('happy'))
