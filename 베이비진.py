e42037ea00ea34409f4073fa57a37f774c3025d6
hyunin3 <stock4share2@gmail.com>
2023-04-10 15:42:24 +0900

    0410

--- 베이비진.py
+++ 베이비진.py
@@ -1,31 +1,27 @@
 def check(lst):
     cnt = 0
-    # [0:3] 또는 [3:6] 값이 run, triplet
     for i in range(0, N, 3):
-        if lst[i]==lst[i+1]==lst[i+2] or lst[i]+2==lst[i+1]+1==lst[i+2]:
-            cnt+=1
-    return cnt==2
- 
+        if lst[i] == lst[i+1] == lst[i+2] or lst[i] + 2 == lst[i+1] + 1 == lst[i+2]:
+            cnt += 1
+    return cnt == 2
+        
 def dfs(n, tlst):
     global ans
-    if n==N:
+    if n == N:
         if check(tlst):
             ans = 1
-        return
- 
+        return    
     for j in range(N):
-        if v[j]==0:
-            v[j]=1
+        if v[j] == 0:
+            v[j] = 1
             dfs(n+1, tlst+[lst[j]])
-            v[j]=0
- 
+            v[j] = 0
+
 T = int(input())
 for tc in range(1, T + 1):
-    lst = list(map(int, input()))
- 
     N = 6
-    v = [0]*N
+    lst = list(map(int, input()))
+    v = [0] * N
     ans = 0
- 
     dfs(0, [])
     print(f'#{tc} {ans}')
